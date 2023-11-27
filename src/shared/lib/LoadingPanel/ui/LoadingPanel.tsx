41702d6e7172170eb2516a03ce9a5f28f7be9241
danilkk <ezzfvkoko@gmail.com>
2023-11-27 15:44:18 +0300

    feat: add LoadingPanel

--- src/shared/lib/LoadingPanel/ui/LoadingPanel.tsx
+++ src/shared/lib/LoadingPanel/ui/LoadingPanel.tsx
@@ -1,8 +1,12 @@
 "use client"
+
 import React, {createContext} from 'react';
-import {useFetch} from "@/shared/hooks/useFetch";
 import Skeleton from 'react-loading-skeleton'
 import 'react-loading-skeleton/dist/skeleton.css'
+
+import {useFetch} from '@/shared/hooks/useFetch';
+
+import styles from './LoadingPanel.module.scss'
 
 type LoadingPanelProps<T> = {
     fetcher: ReturnType<typeof useFetch<T>>;
@@ -13,23 +17,24 @@ type LoadingPanelProps<T> = {
 export const LoadingContext = createContext<any>(undefined)
 const LoadingPanel = <T, >({fetcher, children, count}: LoadingPanelProps<T>) => {
 
-    console.log(fetcher)
-
     // TODO: Придумать что-нибудь с цветами (использовать theme.useToken())
     // TODO: Придумать что-нибудь с размером скелетона
     return <LoadingContext.Provider value={fetcher.data}>
         {fetcher.loading ? <Skeleton height="100px"
                                      baseColor={"#2a689d"}
                                      highlightColor={"#020D71"}
-                                     borderRadius="0.5rem"
+                                     borderRadius="20px"
                                      count={count || 1}
                                      duration={2}/> :
-            fetcher.error ? <Skeleton height="100px"
-                                      baseColor={"#9d2a2a"}
-                                      highlightColor={"#710202"}
-                                      borderRadius="0.5rem"
-                                      count={count || 1}
-                                      duration={2}/> : fetcher.data && children}
+            fetcher.error ? <div className={styles.main}>
+                <h4 className={styles.error}>{fetcher.error}</h4>
+                <Skeleton height="100px"
+                          baseColor={"#9d2a2a"}
+                          highlightColor={"#710202"}
+                          borderRadius="20px"
+                          count={count || 1}
+                          duration={2}/>
+            </div> : fetcher.data && children}
     </LoadingContext.Provider>;
 };
 
