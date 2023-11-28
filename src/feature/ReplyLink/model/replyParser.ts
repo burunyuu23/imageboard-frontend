88725286dbe548adc6e484a3e8a06c632a56d716
danilkk <ezzfvkoko@gmail.com>
2023-11-28 16:08:52 +0300

    fix: replyParser \\

--- src/feature/ReplyLink/model/replyParser.ts
+++ src/feature/ReplyLink/model/replyParser.ts
@@ -3,7 +3,7 @@ import { REPLY_SIGN } from "@/entity/Message";
 import {ParsedReplies} from "./types";
 
 export const replyParse = (messageBody: string): ParsedReplies => {
-    const regex = new RegExp(`${REPLY_SIGN}\s*\d+\s*`, 'g');
+    const regex = new RegExp(`${REPLY_SIGN}\\s*\\d+\\s*`, 'g');
 
     const replies = messageBody.match(regex) || [];
     const text = messageBody.split(regex);
